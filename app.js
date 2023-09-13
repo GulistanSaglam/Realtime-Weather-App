e04362113a7f7bc4a0ef4c35b266541126df4f49
gulistan Saglam <gulistansglm@gmail.com>
2023-09-14 02:43:20 +0300

    second commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const result = document.querySelector('#result');
+const searchButton = document.querySelector('#search-btn');
+
+
+
+async function getWeather() {
+
+    const country = document.querySelector('#country').value;
+
+    if (country == '')
+        return;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'b97c87c0f6mshc1060d10220bde9p136c10jsnb88d095a0f65',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+
+    await fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${country}`, options)
+        .then(response => response.json())
+        .then(veri => {
+
+            if(veri.code === 1006 || veri.code === 400){
+                alert('No matching location found.');
+            }
+            console.log(veri);
+            console.log(veri.location.country);
+            console.log(veri.current.temp_c);
+
+            result.innerHTML = `<span>${veri.location.country}, ${veri.current.temp_c} Celcius Degree</span>                            
+                                `;
+
+        })
+        .catch((error) => {
+            console.log(error);
+            result.innerHTML = `<span>Oops! No matching location found. Please enter again.</span>                            
+            `;
+        });
+
+
+
+}
+
+
+searchButton.addEventListener('click', getWeather);
